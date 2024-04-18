e1687ecd9b630097ca3b3c3784a424984bbaf50d
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-04-18 18:48:44 +0530

    handleShowAddFriend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,13 +21,27 @@ const initialFriends = [
   },
 ];
 
+const Button = ({ children, onClick }) => {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
 const App = () => {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
@@ -65,10 +81,6 @@ const Friend = ({ friend }) => {
       <Button>Select</Button>
     </li>
   );
-};
-
-const Button = ({ children }) => {
-  return <button className="button">{children}</button>;
 };
 
 const FormAddFriend = () => {
